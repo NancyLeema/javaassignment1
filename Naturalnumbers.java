4b3ac416c41ac8eec313d91b9f48287e8204661e
NancyLeema <85402224+NancyLeema@users.noreply.github.com>
2023-06-25 15:26:15 +0530

    Add files via upload

---
+++ Naturalnumbers.java
@@ -0,0 +1,21 @@
+package org.javapro5;
+
+
+
+public class Naturalnumbers {
+
+	public static void main(String[] args) {
+		int num=100;
+		int sum=0;
+		for(int i=1;i<=num;i++) {
+		 sum=sum+i;
+		
+				
+		
+		// TODO Auto-generated method stub
+
+	}
+		System.out.println("sum of first 100 natural numbers="+ sum);
+		
+
+}}
