3352d50808328014a2d84e5969e825fb42b8830c
NancyLeema <85402224+NancyLeema@users.noreply.github.com>
2023-06-25 15:24:51 +0530

    Add files via upload

---
+++ Sampleprogm.java
@@ -0,0 +1,13 @@
+package org.javapro3;
+
+public class Sampleprogm {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			System.out.println("Hello World Java");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
