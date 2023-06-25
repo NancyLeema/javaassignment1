94ba61babeaab8fb88b3f9739052c6a8b8b597e8
NancyLeema <85402224+NancyLeema@users.noreply.github.com>
2023-06-25 15:24:01 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+package org.javapro2;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=0;j<=(i-1);j++) {
+			
+				System.out.print(1);
+		}
+		// TODO Auto-generated method stub
+			System.out.println();
+			
+		
+	}
+
+}}
