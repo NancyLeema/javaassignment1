5fcdf30468a9435f52040156967787ad87b6b508
NancyLeema <nancyleema.k0911@gmail.com>
2023-06-25 15:20:52 +0530

    initial commit

---
+++ Patternprogram.java
@@ -0,0 +1,21 @@
+package org.javaproject1;
+
+public class Patternprogram {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print( j);
+				
+			}
+			System.out.println();
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
